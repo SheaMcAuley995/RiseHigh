3d929f1b8d8a5dfbaf560a9e12ac70f158dfee11
Shea McAuley <s179049@students.aie.edu.au>
2018-08-22 16:45:38 -0700

    First commit

---
+++ Assets/BaseMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMovement : MonoBehaviour {
+
+    Camera cam;
+    Rigidbody2D rb;
+    public GameObject bullet;
+    public Transform shootPos;
+    public float movementSpeed;
+
+	void Start () {
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+
+	void Update () {
+
+        float camDis = cam.transform.position.y - transform.position.y;
+
+        Vector3 mouse = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, camDis));
+
+        float angleRad = Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x);
+        float angle = (180 / Mathf.PI) * angleRad;
+
+        rb.rotation = angle;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.right * movementSpeed,ForceMode2D.Force);
+        }
+        if(Input.GetMouseButton(0))
+        {
+            Instantiate(bullet,shootPos.position, shootPos.rotation);
+        }
+        
+	}
+}
---
+++ Assets/bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+
+    Rigidbody2D rb;
+    private float movementSpeed = 45;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	void Update () {
+        rb.AddForce(transform.right * movementSpeed, ForceMode2D.Force);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Wall")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
